b3b2c0fce13ebf5c7bfbf94412d3b284a1d1bd9a
em-em-D <michaeldamulak@gmail.com>
2019-07-24 13:14:10 +0200

    user rendering not running

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def new
+     @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+      flash[:success] = "Welcome to the Refill!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def index
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,:password_confirmation)
+  end
+end
